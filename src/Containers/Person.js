0aa1c2af4b1d047d838fb9f15afde4a11cde3c29
ucaboro <uaboro@ucl.ac.uk>
2018-03-21 22:59:09 +0000

    Personal page Avatar+2 cards (responsive)

--- src/Containers/Person.js
+++ src/Containers/Person.js
@@ -2,15 +2,65 @@ import React from 'react';
 import withAuthorization from '../Components/withAuthorization';
 import PropTypes from 'prop-types';
 import  PasswordForgetPage  from './passwordForget';
+import {Row, Col, Grid} from 'react-bootstrap';
+import Avatar from 'material-ui/Avatar';
+import FontIcon from 'material-ui/FontIcon';
+
+const AvatarStyle = {
+  border: 0,
+  objectFit: 'cover',
+}
 
 
 const Person = (props, {authUser}) =>
+<div>
+  <header style={{height: '200px'}} className="App-header"/>
+
+
+  <Row >
+    <Col sm={12} mdHidden  lgHidden style={{marginTop: '-195px'}} >
+    <Avatar
+     src="https://naqyr37xcg93tizq734pqsx1-wpengine.netdna-ssl.com/wp-content/uploads/2016/01/76-Intelligent-Richard-Branson-Quotes.jpg"
+     size={100}
+     style={AvatarStyle}
+      />
+    </Col>
+  </Row>
+
+<Row style={{marginTop: '-95px'}}>
+  <Col lg={5} md={4} sm={6} className='InvestedSheetCol'>
+    <div className='card card-1'>
+      <p>hey</p>
+    </div>
+  </Col>
+
+  <Col xsHidden smHidden lg={2} md={4} className='AvatarCol'>
+
+    <Avatar
+     src="https://naqyr37xcg93tizq734pqsx1-wpengine.netdna-ssl.com/wp-content/uploads/2016/01/76-Intelligent-Richard-Branson-Quotes.jpg"
+     size={200}
+     style={AvatarStyle}
+      />
+
+  </Col>
+
+  <Col   lg={5} md={4} sm={6} className='ProfitSheetCol'>
+    <div className='card card-1'>
+      <p>hey2</p>
+    </div>
+  </Col>
+
+</Row>
+
 
   <div>
     <h1>Account: {authUser.email} </h1>
     <p>The Account Page is accessible by every signed in user.</p>
     < PasswordForgetPage />
   </div>
+
+
+</div>
 
 Person.contextTypes = {
   authUser: PropTypes.object,
