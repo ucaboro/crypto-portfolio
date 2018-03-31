bbaaf1d4cce39cd23f3a6dc47b162d373d9f3bc8
ucaboro <uaboro@ucl.ac.uk>
2018-03-31 07:46:29 +0100

    Cryptocurrency list with icons, infinite loading and a button placeholder to add to the card

--- src/Components/expandableCoinList.js
+++ src/Components/expandableCoinList.js
@@ -6,90 +6,259 @@ import {
   TableHeader,
   TableHeaderColumn,
   TableRow,
-  TableRowColumn,
+  TableRowColumn
 } from 'material-ui/Table';
 import Divider from 'material-ui/Divider';
+import Infinite from 'react-infinite'
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import {grey400} from 'material-ui/styles/colors';
+import IconAddButton from 'material-ui/svg-icons/content/add-circle';
+import * as Icon from 'react-cryptocoins';
+import CircularProgress from 'material-ui/CircularProgress';
 
-export default class ExpandableCoinList extends Component {
-  constructor(){
-    super();
-    this.state = {
-      expanded: '0px',
-      buttonLabel: 'List All Cryptocurrencies',
-    }
+import {List, ListItem} from 'material-ui/List';
 
-    this.expandCoinList = this.expandCoinList.bind(this)
+const iconButtonElement = (
+  <IconButton
+    touch={true}
+    tooltip="Add to card"
+    tooltipPosition="bottom-left"
+  >
+    <IconAddButton color={grey400} />
+  </IconButton>
+);
+
+
+class CryptoIcon extends Component{
+  constructor(props){
+    super(props);
   }
 
-  expandCoinList(){
-    if(this.state.expanded==='0px'){
-    this.setState({
-      expanded: '1000px',
-      buttonLabel: 'Collapse the list'
-    })
-  }else{
-    this.setState({
-      expanded: '0px',
-      buttonLabel: 'List All Cryptocurrencies'
-
-    })
-  }
-
-  }
 
   render(){
-    return (
-    <div>
-      <FlatButton label={this.state.buttonLabel} primary={true} onClick={this.expandCoinList} style={{marginTop:'10px'}}/>
-      <div className="center-block" id="expandableList" style={{maxHeight: this.state.expanded, maxWidth: this.state.expanded}}>
-        <TableExampleSimple/>
-      </div>
-    </div>
+    var lowCase = this.props.string.toLowerCase();
+    var firstLetterCap = "Icon."+lowCase.charAt(0).toUpperCase() + lowCase.slice(1);
+
+    return(
+    `<Icon.Btc/>`
     )
   }
 }
 
-function colorNum(num){
-  if(num.charAt(0)==='-'){
+
+
+export default class ExpandableCoinList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      expanded: '0px',
+      buttonLabel: 'List All Cryptocurrencies',
+      elements: this.buildElements(0, 15),
+      isInfiniteLoading: false
+    }
+    this.handleInfiniteLoad = this.handleInfiniteLoad.bind(this);
+    this.expandCoinList = this.expandCoinList.bind(this)
+  }
+   IconCapitalizeFirstLetter(string) {
+     var lowCase = string.toLowerCase();
+     var firstLetterCap = lowCase.charAt(0).toUpperCase() + lowCase.slice(1);
+    return firstLetterCap
+}
+
+    stringToIcon(string){
+
+        switch(string) {
+          case ('BTC'):
+          return <Icon.Btc/>;
+
+          case ('ETH'):
+          return <Icon.Eth/>;
+
+            case ('LTC'):
+            return <Icon.Ltc/>;
+
+              case ('EOS'):
+              return <Icon.Eos/>;
+
+                case ('ADA'):
+                return <Icon.Ada/>;
+        }
+
+      }
+
+
+
+  expandCoinList() {
+    if (this.state.expanded === '0px') {
+      this.setState({expanded: '1000px', buttonLabel: 'Collapse the list'})
+    } else {
+      this.setState({expanded: '0px', buttonLabel: 'List All Cryptocurrencies'})
+    }
+
+  }
+
+  buildElements(start, end) {
+    var elements = [];
+    console.log(this.IconCapitalizeFirstLetter("BTC"))
+    if (end < tableData.length) {
+      for (var i = start; i < end; i++) {
+        elements.push(
+
+
+          <ListItem
+            leftAvatar={<p style={{verticalAllign: 'middle'}}>{i+1} &nbsp; {this.stringToIcon(tableData[i].coin)}   </p>}
+            rightIconButton={iconButtonElement}
+            primaryText={tableData[i].coin}
+            secondaryText={tableData[i].price}
+            className="center-block"
+            style={{display:"table-row"}}
+            key={i}>
+
+          </ListItem>
+
+        )
+      }
+    }
+    return elements;
+
+  }
+
+  handleInfiniteLoad() {
+    if (this.state.elements.length < tableData.length - 15) {
+
+      this.setState({isInfiniteLoading: true});
+      var that = this;
+      setTimeout(function() {
+        var elemLength = that.state.elements.length,
+          newElements = that.buildElements(elemLength, elemLength + 10);
+        that.setState({isInfiniteLoading: false, elements: that.state.elements.concat(newElements)});
+
+      }, 1500);
+
+    }
+
+    console.log(this.state.elements.length + " " + tableData.length)
+
+  }
+
+  elementInfiniteLoad() {
+    return <div className="infinite-list-item" style={{marginLeft: '15px'}}>
+      <CircularProgress color={grey400}  size={30}/>
+    </div>;
+  }
+
+  render() {
+
+    return (
+                  <div>
+      <FlatButton label={this.state.buttonLabel} primary={true} onClick={this.expandCoinList} style={{
+          marginTop: '10px'
+        }}/>
+
+
+      <div className="center-block" id="expandableList" style={{
+          maxHeight: this.state.expanded,
+          maxWidth: this.state.expanded
+        }}>
+
+        <Infinite
+                      elementHeight={50}
+                      containerHeight={400}
+                      infiniteLoadBeginEdgeOffset={100}
+                      onInfiniteLoad={this.handleInfiniteLoad}
+                      loadingSpinnerDelegate={this.elementInfiniteLoad()}
+                      isInfiniteLoading={this.state.isInfiniteLoading}>
+
+          {this.state.elements}
+
+      </Infinite>
+
+      </div>
+
+    </div>
+  )
+  }
+}
+
+function colorNum(num) {
+  if (num.charAt(0) === '-') {
     return '#B71C1C'
-  } else{
+  } else {
     return '#388E3C'
   }
 }
 
 
-const TableExampleSimple = () => (
-  <Table>
-    <TableHeader>
-      <TableRow>
-        <TableHeaderColumn>Rank</TableHeaderColumn>
-        <TableHeaderColumn>Coin</TableHeaderColumn>
-        <TableHeaderColumn>MarketCap</TableHeaderColumn>
-        <TableHeaderColumn>Price</TableHeaderColumn>
-        <TableHeaderColumn>Change {'24hr'}</TableHeaderColumn>
-      </TableRow>
-    </TableHeader>
-    <TableBody>
-      {tableData.map( (row, index) => (
-              <TableRow key={index}>
-                <TableRowColumn>{index+1}</TableRowColumn>
-                <TableRowColumn>{row.coin}</TableRowColumn>
-                <TableRowColumn>{row.marketCap}</TableRowColumn>
-                <TableRowColumn>{row.price}</TableRowColumn>
-                <TableRowColumn style={{color:colorNum(row.change)}}>{row.change}</TableRowColumn>
-              </TableRow>
-              ))}
-    </TableBody>
-  </Table>
-);
 
-function addEmoji(num){
-  if(num>0){
-  return '+'+num+'%'+'📈'
-} else {
-  return num+'%'+'📉'
+
+
+
+
+
+
+
+class ListExampleSimple extends Component {
+  constructor() {
+    super();
+    this.state = {
+      elements: this.buildElements(0, 15),
+      isInfiniteLoading: false
+    }
+    this.handleInfiniteLoad = this.handleInfiniteLoad.bind(this);
+  }
+
+  buildElements(start, end) {
+    var elements = [];
+    if (end < tableData.length) {
+      for (var i = start; i < end; i++) {
+        elements.push(
+          <List key={i}>{i} {tableData[i].coin} {tableData[i].price} {tableData[i].marketCap} {tableData[i].change}</List>
+
+)
+      }
+    }
+    return elements;
+
+  }
+
+  handleInfiniteLoad() {
+    if (this.state.elements.length < tableData.length - 15) {
+
+      this.setState({isInfiniteLoading: true});
+      var that = this;
+      setTimeout(function() {
+        var elemLength = that.state.elements.length,
+          newElements = that.buildElements(elemLength, elemLength + 10);
+        that.setState({isInfiniteLoading: false, elements: that.state.elements.concat(newElements)});
+
+      }, 2500);
+
+    }
+
+    console.log(this.state.elements.length + " " + tableData.length)
+
+  }
+
+  elementInfiniteLoad() {
+    return <div className="infinite-list-item">
+      Loading...
+    </div>;
+  }
+  render() {
+    return (<div>
+      {this.state.elements}
+    </div>)
+  }
 }
 
+function addEmoji(num) {
+  if (num > 0) {
+    return '+' + num + '%' + '📈'
+  } else {
+    return num + '%' + '📉'
+  }
 
 }
 
@@ -99,18 +268,446 @@ const tableData = [
     coin: 'BTC',
     price: '10,000$',
     marketCap: '125,862,406,273.35',
-    change: addEmoji(-1.2),
-  },
-  {
+    change: addEmoji(-1.2)
+  }, {
     coin: 'ETH',
     price: '800$',
     marketCap: '125,862,406,273.35',
-    change: addEmoji(+3.1),
-  },
-  {
+    change: addEmoji(+ 3.1)
+  }, {
     coin: 'EOS',
     price: '100$',
     marketCap: '125,862,406,273.35',
-    change: addEmoji(-4.2),
-  },
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ADA',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'LTC',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: '16!EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }, {
+    coin: 'ETH',
+    price: '800$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(+ 3.1)
+  }, {
+    coin: 'EOS',
+    price: '100$',
+    marketCap: '125,862,406,273.35',
+    change: addEmoji(-4.2)
+  }
 ];
